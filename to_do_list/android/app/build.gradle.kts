33b10b5f7019b3bb09a7b98aa7699e30e44bedbd
TakSofpalo <nabievnikita2004@mail.ru>
2026-05-05 22:02:42 +0300

    Релиз приложения

--- to_do_list/android/app/build.gradle.kts
+++ to_do_list/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.to_do_list"
+    namespace = "ru.flutterlab.todolist"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -20,8 +20,8 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.to_do_list"
+        // Application ID для публикации в Google Play Store
+        applicationId = "ru.flutterlab.todolist"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- to_do_list/android/app/src/main/kotlin/ru/flutterlab/todolist/MainActivity.kt
+++ to_do_list/android/app/src/main/kotlin/ru/flutterlab/todolist/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.to_do_list
+package ru.flutterlab.todolist
 
 import io.flutter.embedding.android.FlutterActivity
 
